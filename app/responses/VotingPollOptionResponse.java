3257c407088298c84ec418b908a382bcb9c0a801
Oliver Dozsa <dozsa.oliver@gmail.com>
2022-10-20 20:49:14 +0200

    Removed obsolete field from poll option response.

--- app/responses/VotingPollOptionResponse.java
+++ app/responses/VotingPollOptionResponse.java
@@ -1,17 +1,8 @@
 package responses;
 
 public class VotingPollOptionResponse {
-    private Integer index;
     private String name;
     private Integer code;
-
-    public Integer getIndex() {
-        return index;
-    }
-
-    public void setIndex(Integer index) {
-        this.index = index;
-    }
 
     public String getName() {
         return name;
