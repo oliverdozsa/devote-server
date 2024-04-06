bebcde63c5839258490a766d50a5bcfb17eefb85
Oliver Dozsa <dozsa.oliver@gmail.com>
2024-04-06 16:53:00 +0200

    Fixed web3 storage.
    (cherry picked from commit 318194536271286f6f1579efd6cb6d8fa5db797e)

--- app/ipfs/api/imp/Web3StorageIpfsApiImp.java
+++ app/ipfs/api/imp/Web3StorageIpfsApiImp.java
@@ -12,26 +12,30 @@ import java.util.concurrent.CompletionStage;
 
 public class Web3StorageIpfsApiImp implements IpfsApi {
     private WSClient wsClient;
-    private String jwt;
+    private String token;
 
     @Inject
     public Web3StorageIpfsApiImp(WSClient wsClient, Config config) {
         this.wsClient = wsClient;
-        this.jwt = config.getString("galactic.host.ipfs.web3storage.jwt");
+        this.token = config.getString("galactic.host.ipfs.web3storage.token");
     }
 
     @Override
     public String saveJson(JsonNode json) {
-        CompletionStage<WSResponse> response = this.wsClient.url("https://api.web3.storage/upload")
+        CompletionStage<WSResponse> response = this.wsClient.url("https://bridge.galactic.pub/voting-ipfs")
                 .addHeader("Content-Type", "application/json")
-                .addHeader("Authorization", "Bearer " + jwt)
+                .addHeader("Authorization", "Bearer " + token)
                 .post(json.toString());
 
         try {
             return response
                     .thenApply(r -> {
-                        JsonNode responseJson = r.asJson();
-                        return responseJson.get("cid").asText();
+                        if(r.getStatus() < 200 && r.getStatus() >= 300) {
+                            throw new BlockchainException("Failed to store through bridge! status = " + r.getStatus());
+                        }
+
+                        String locationHeader = r.getSingleHeader("Location").get();
+                        return parseCidFromUrl(locationHeader);
                     })
                     .toCompletableFuture().get();
         } catch (Exception e) {
@@ -41,7 +45,7 @@ public class Web3StorageIpfsApiImp implements IpfsApi {
 
     @Override
     public JsonNode retrieveJson(String cid) {
-        CompletionStage<WSResponse> respone = this.wsClient.url("https://w3s.link/ipfs/" + cid)
+        CompletionStage<WSResponse> respone = this.wsClient.url("https://" + cid + ".ipfs.w3s.link")
                 .addHeader("Content-Type", "application/json")
                 .get();
 
@@ -53,5 +57,10 @@ public class Web3StorageIpfsApiImp implements IpfsApi {
         } catch (Exception e) {
             throw new BlockchainException("Failed to get json in IPFS through Web3Storage.", e);
         }
+    }
+
+    private static String parseCidFromUrl(String url) {
+        String cidPart = url.split("\\.")[0];
+        return cidPart.split("https://")[1];
     }
 }
